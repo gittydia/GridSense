b44ac6351689b27940e8f34feaa2279494348c2d
Dianne Boholst <yeogiyeogi394@gmail.com>
2025-03-05 20:19:41 +0800

    Update Vite configuration to add path aliasing for src directory

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  root: './client',
   plugins: [react()],
-  server: {
-    port: 3000
-  }
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
